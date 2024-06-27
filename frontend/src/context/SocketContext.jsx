db9782c713f40a03452f7abad3393ad7bf40775b
Abhinav072004 <158208515+Abhinav072004@users.noreply.github.com>
2024-06-27 15:54:51 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://i-chat-app-6bdb.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
